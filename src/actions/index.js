67b31af207d44dcb4a7592ccfb709d73f687f731
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-07-28 19:55:17 +0530

    add a simple reducer

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+// {
+//     type: 'ADD_MOVIES', //necessary to tell the action what to do
+//     movies:[m1,m2,m3]
+// }
+// {
+//     type:''
+// }
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+export default function movies(state = [], action) {
+	//state shows the current state of the store
+	if (action.type == 'ADD_MOVIES') {
+		return action.movies;
+	}
+	return state; //a reducer has to return something always so be careful
+}
+
+// this reducer is a pure function which neither imports anything out of the argument nor changes the DOM it just returns a value
+//NOTE: reducer always returns the new state does not modify the current state,and then the new state is merged ny the store itself
